fa1a96f0002afd85f8b82b0a8f6ea4ef352991e6
Evren Ispiroglu <evrenn.ispiroglu@gmail.com>
2023-01-24 22:25:09 +0300

    docker compose can build now :)

---
+++ src/main/java/dev/ispiroglu/jobadvertysystem/controller/TestController.java
@@ -0,0 +1,16 @@
+package dev.ispiroglu.jobadvertysystem.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
+@Slf4j
+public class TestController {
+    @GetMapping
+    public void log() {
+        log.info("Test Log");
+    }
+}
